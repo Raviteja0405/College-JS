7602c4d2343f27a1b96fd8e26659dc478002747e
Raviteja0405 <raviteja40025040@gmail.com>
2023-10-06 21:44:06 +0530

    added controlstatements.js

--- control-statements.js
+++ control-statements.js
@@ -11,3 +11,38 @@ if (a > b) {
   // else block
   console.log("b is big");
 }
+
+//Switch Statements
+// let choice = 4;
+
+// switch (choice) {
+//   case 1:
+//     console.log("first case");
+//     break;
+//   case 2:
+//     console.log("second case");
+//     break;
+//   case 3:
+//     console.log("Third case");
+//     break;
+//   default:
+//     console.log("Invalid Choice");
+// }
+
+//Arithmetic operations
+let l = 10;
+let m = 20;
+let choice = 1;
+
+switch (choice) {
+    case 1:
+        let sum =l+m;
+      console.log("sum : ", sum);
+      break;
+    case 2:
+        let diff = l-m;
+      console.log("difference: ", diff);
+      break;
+    default:
+      console.log("Invalid Choice");
+  }
