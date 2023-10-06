f1b0635df45aea10cc0ed581fd4c46da19a17745
Raviteja0405 <raviteja40025040@gmail.com>
2023-10-06 20:05:50 +0530

    added content into variables.js

--- variables.js
+++ variables.js
@@ -1,1 +1,21 @@
-console.log("Hello world");
+// Variables
+
+// Js is dynamically typed language -> datatype is allocated dynamically.
+let a;
+console.log("The value of a is ", a);
+console.log("The datatype of a is ", typeof a);
+
+//assign data to a
+a = 100;
+console.log("The value of a is ", a);
+console.log("The datatype of a is ", typeof a);
+
+a = "hello";
+console.log("The value of a is ", a);
+console.log("The datatype of a is ", typeof a);
+
+a = true;
+console.log("The value of a is ", a);
+console.log("The datatype of a is ", typeof a);
+
+//In JS, Variables don't have types - Values have types
