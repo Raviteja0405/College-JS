96f09e9e83514fb020a5e413d91d6e7308d8ed78
Raviteja0405 <raviteja40025040@gmail.com>
2023-10-06 20:13:10 +0530

    added control-statements

---
+++ control-statements.js
@@ -0,0 +1,13 @@
+//compare two values
+let a = 100;
+let b = 100;
+
+if (a > b) {
+  // if block
+  console.log("a is big");
+} else if (a == b) {
+  console.log("both are equal");
+} else {
+  // else block
+  console.log("b is big");
+}
