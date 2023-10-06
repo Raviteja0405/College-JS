ba5ca408d51a391adf414c95f57d0fb253230327
Raviteja0405 <raviteja40025040@gmail.com>
2023-10-06 20:09:27 +0530

    added operators.js

---
+++ operators.js
@@ -0,0 +1,29 @@
+let a = 10;
+let b = 4;
+
+//exponentiation
+let result = a ** b;
+console.log("result is", result);
+
+//remainder
+let rm = a % b;
+console.log("remainder is ", rm);
+
+// a = a + 20;
+a += 20; //compound addition
+console.log(a);
+
+// console.log(a==b) //equal
+// console.log(a===b);//strict equal
+
+let x = 123;
+let y = "123";
+
+console.log(x == y);
+console.log(x === y);
+
+
+//Logical
+console.log(x > 100 && y < 200); //true
+
+//Rest operators are already known in courses like 'Programming in C'.
