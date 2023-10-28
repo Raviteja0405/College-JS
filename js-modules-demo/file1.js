7e735a140de4b44197ef059cbca50f3cb16359ab
Raviteja0405 <raviteja40025040@gmail.com>
2023-10-28 22:15:34 +0530

    added named export and default export- initials

--- js-modules-demo/file1.js
+++ js-modules-demo/file1.js
@@ -1,3 +1,15 @@
-export let a = 10;
+// //export
 
-//export
+
+// //named export
+
+// // export let a = 10;
+// let a = 10;
+
+// // export let names = ['ravi', 'kiran', 'babu']
+// let names = ['ravi', 'kiran', 'babu']
+
+// // export{a, names}// this is also possible 
+
+// //default export
+// export default {a, names};
