6dec4a7111f15a8886461611a51db0a5d5be1fb2
Raviteja0405 <raviteja40025040@gmail.com>
2023-10-28 22:15:57 +0530

    added named and default import - initials

--- js-modules-demo/file2.js
+++ js-modules-demo/file2.js
@@ -1,4 +1,8 @@
 //import
-import { a } from "./file1.js";
+// import { a, names } from "./file1.js"; //while in named export
+import x from './file1.js' //while in default export
 
-console.log(a);
+console.log(`value of a is `,x);
+// console.log(names);
+console.log(x.a);
+console.log(x.names);
