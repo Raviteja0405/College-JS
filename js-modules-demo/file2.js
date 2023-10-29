635bb54a9a9e7fe22f0707bca0bab49680dbd51b
Raviteja0405 <raviteja40025040@gmail.com>
2023-10-29 19:49:40 +0530

    modified file2.js

--- js-modules-demo/file2.js
+++ js-modules-demo/file2.js
@@ -1,8 +1,11 @@
 //import
 // import { a, names } from "./file1.js"; //while in named export
-import x from './file1.js' //while in default export
+// import {x} from './file1.js' //while in default export
+import { student } from "./student.js";
 
-console.log(`value of a is `,x);
-// console.log(names);
-console.log(x.a);
-console.log(x.names);
+// console.log(`value of a is `,x);
+// // console.log(names);
+// console.log(x.a);
+// console.log(x.names);
+
+console.log(`value of stu in file2 is `,student);
