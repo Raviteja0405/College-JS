bfc3c8b255f337130253bb3ffeb6217f8bae80dd
Raviteja0405 <raviteja40025040@gmail.com>
2023-11-16 20:30:42 +0530

    setTimeout and setInterval

--- async-js-demo/async.js
+++ async-js-demo/async.js
@@ -1,3 +1,44 @@
-let ar = [1,2,3,4,5,6,7];
+// console.log("Three customers reached restaurant");
 
-let result = ar.filter(element=> element<5)
+//synchronous
+// console.log("Cust - 1 ordered biryani which takes 15 min to prepare");
+// for(let x = 1; x<900000;x++){}
+// console.log("custome-1 received biryani");
+
+// console.log("Cust - 2 ordered biryani which takes 15 min to prepare");
+// for(let x = 1; x<600000;x++){}
+// console.log("custome-2 received biryani");
+
+// console.log("Cust - 3 ordered biryani which takes 15 min to prepare");
+// for(let x = 1; x<700000;x++){}
+// console.log("custome-3 received biryani");
+
+
+
+
+
+
+
+//asynchronously
+
+// console.log("Cust - 1 ordered biryani which takes 15 min to prepare");
+// setTimeout(() => {
+//     console.log("custome-1 received biryani");
+// }, 7000);
+
+// console.log("Cust - 2 ordered biryani which takes 15 min to prepare");
+// setTimeout(() => {
+//     console.log("custome-2 received biryani");
+// }, 5000);
+
+// console.log("Cust - 3 ordered biryani which takes 15 min to prepare");
+// setTimeout(() => {
+//     console.log("custome-3 received biryani");
+// }, 2000);
+
+
+// setInterval(() => {
+//     console.log("hellow"); // this will repeat 1 sec 
+// }, 1000);
+
+console.log("first");
