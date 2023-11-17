4d103252626a2c4f979d1539e93daf947884def0
Raviteja0405 <raviteja40025040@gmail.com>
2023-11-17 21:06:51 +0530

    add api-call.js

---
+++ api-call-demo/api-call.js
@@ -0,0 +1,10 @@
+// // make API req(HTTP Req and AJAX request)
+// fetch("https://jsonplaceholder.typicode.com/posts")
+// .then(response=>response.json())
+// .then(posts=>console.log(posts))
+// .catch(err => console.log('Err in API Call',err))
+
+
+//fetch returns a promise
+// console.log(fetch("https://jsonplaceholder.typicode.com/posts"))
+
