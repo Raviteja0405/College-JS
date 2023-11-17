8201acb5dfa20bdb98a7cd54093785326ad85c7c
Raviteja0405 <raviteja40025040@gmail.com>
2023-11-17 20:52:42 +0530

    promise

--- async-js-demo/promise-demo.js
+++ async-js-demo/promise-demo.js
@@ -18,10 +18,33 @@ let futureCondition = true;// condition through which future is being decided
         },5000);
     })
 
-//Consume promise(Your Frnd)
-    phoneCallPromise // then when catch fulfilled
-    .then((successMessage)=>{console.log(successMessage)})
+// //Consume promise(Your Frnd)
+//     phoneCallPromise // then when promise fulfilled
+//     .then((successMessage)=>{console.log(successMessage)})
+//     .catch((errorMessage)=>{console.log(errorMessage);})
+
+    phoneCallPromise // then when promise fulfilled
+    .then((successMessage)=>{return successMessage}) // it returns a promise again
+    .then((msg)=>console.log("message in second then is",msg))
+    // .then() if again second then is returning a promise
     .catch((errorMessage)=>{console.log(errorMessage);})
 
-
 // console.log("The last action");
+
+
+// -----------JSON------------------
+// js obj
+let obj = {
+    firstName: 'ravi',
+    secondName: 'baby',
+    age:21
+}
+//convert obj to JSON
+let jsonData = JSON.stringify(obj)
+console.log(jsonData);
+
+// {
+//     "firstName":"ravi",
+//     "secondName":"baby",
+//     "age":21
+// }
