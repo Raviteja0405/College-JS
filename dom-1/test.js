abd8d9b507e3c42f1beca5f90bce87553d3fc3cc
Raviteja0405 <raviteja40025040@gmail.com>
2023-11-22 19:44:12 +0530

    dom-test.js

---
+++ dom-1/test.js
@@ -0,0 +1,26 @@
+//Access elements from html
+
+//Access head
+// let headElement = document.head;
+// console.log(headElement);
+
+//Access body
+// let bodyElement = document.body;
+// console.log(bodyElement);
+
+//Access other elements
+// h1
+// let h1 = document.getElementById("heading");
+// console.log(h1);
+
+// let h1a = document.getElementsByTagName("h1")
+// console.log(h1a);
+
+// let h1b= document.querySelector("#heading")
+// console.log(h1b);
+
+// let div = document.querySelector(".main")
+// console.log(div);
+
+let h1b= document.querySelectorAll("h1")
+console.log(h1b);
